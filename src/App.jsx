08a8f846ca86428393073f1167bff03ca5d449cd
leogenuit <genuitleo@gmail.com>
2024-06-10 16:22:47 +0200

    install axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+const App = () => {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState();
 
-function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://site--deliveroo--jb29wjf8x9mr.code.run/"
+        );
+        console.log(response.data);
+        setData(response.data);
+        console.log("oui");
+        setIsLoading = false;
+        console.log(data); // ici
+      } catch (error) {
+        console.log(error.response);
+      }
+    };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    fetchData();
+  }, []);
 
-export default App
+  return isLoading ? (
+    <span>En cours de chargement ...</span>
+  ) : (
+    <div>
+      {/* {data.map((categorie) => {
+        return (
+          <div>
+            <h2>{categorie.name}</h2>
+          </div>
+        );
+      })} */}
+      hello world
+    </div>
+  );
+};
+
+export default App;
